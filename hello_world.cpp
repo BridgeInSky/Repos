5c206353549b1a1aeb7802028e572502f280713f
BridgeInSky <brizhanskaializa@gmail.com>
2025-03-09 13:49:48 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 int main() {
     string name;
     std::cout << "Enter your name: ";
-    std::cin >> name;//делаем ввод имени пользователем
-    std::cout << "Hello world from " << name << "!" << std::endl;//выводим приветствие с именем
+    std::cin >> name;//We enter the user name
+    std::cout << "Hello world from " << name << "!" << std::endl;//we display a greeting with the name
     return 0;
 }
